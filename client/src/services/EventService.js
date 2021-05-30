6aaed8fb88a836c09786075a35b80d5aac48904e
Teerapat Thong-O <teerapat@gopomelo.com>
2021-05-30 16:35:47 +0700

    call api from api gate way

--- client/src/services/EventService.js
+++ client/src/services/EventService.js
@@ -2,8 +2,9 @@ import axios from "axios"
 
 export default {
   async getEvents() {
-    let res = await axios.get("http://localhost:8000/events");
-    return res.data;
+    // let res = await axios.get("http://localhost:8000/events");
+    let res = await axios.get("http://localhost:8000/api-party/party");
+    return res.data.data || [];
   },
   async getEventSingle(eventId, accessToken) {
     let res = await axios.get("http://localhost:8000/events/" + eventId, {
